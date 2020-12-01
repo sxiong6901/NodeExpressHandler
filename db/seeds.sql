86e025506940111827ffbc720214e452869a629e
sxiong6901 <soua.p.xiong@hotmail.com>
2020-11-30 19:58:03 -0500

    Update

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('McChicken');
+INSERT INTO burgers (name) VALUES ('Double Cheeseburger');
+INSERT INTO burgers (name, devoured) VALUES ('Big Mac', true);
+INSERT INTO burgers (name, devoured) VALUES ('Quarter Pounder', true);
+INSERT INTO burgers (name, devoured) VALUES ('Double Quarter Pounder', true);
+INSERT INTO burgers (name) VALUES ('Big and Tasty');
